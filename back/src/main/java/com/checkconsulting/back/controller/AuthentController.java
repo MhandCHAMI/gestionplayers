76b27271612d8cd7f147b704124bf7a0e9475bc0
mahdi chekini <me.chekini@gmail.com>
2019-06-02 17:41:07 +0200

    add authent service

---
+++ back/src/main/java/com/checkconsulting/back/controller/AuthentController.java
@@ -0,0 +1,17 @@
+package com.checkconsulting.back.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("authent")
+public class AuthentController {
+
+
+    @GetMapping("/isAuthenticated")
+    public String isAuthenticated() {
+        return "test ok";
+    }
+}
